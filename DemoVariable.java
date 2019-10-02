9fc5b36563acc6ae998905500600082ee9539fac
Ghilman <ghilman6@gmail.com>
2019-10-02 19:35:33 +0700

    tugas1

---
+++ DemoVariable.java
@@ -0,0 +1,87 @@
+import java.util.Scanner; 
+public class DemoVariable{
+    public static void main(String[] args) {
+        //Deklarasikan Variable
+        //No 4 & 5
+/*     
+         int a=5;
+        int b=3;
+        int c=a/b;
+        float d= (float)a/(float)b;
+        String e="hello";
+     char f='1';
+     
+     System.out.println("Nilai  a "+a);
+     System.out.println("Nilai  b "+b);
+     System.out.println("Nilai  c "+c);
+     System.out.println("Nilai  d "+d);
+     System.out.println("Nilai  f "+f);*/
+    
+     double s,j,v,g,t,m1,m2,p,a,pv,inte,yrs,fv,c,x,b;
+     Scanner masuk = new Scanner(System.in);
+     
+     //No 6 A input
+/*
+     System.out.print("Masukan J = ");
+     j = masuk.nextDouble();
+     System.out.print("Masukan V = ");
+     v = masuk.nextDouble();
+     System.out.print("Masukan T = ");
+     t = masuk.nextDouble();
+     System.out.print("Masukan G = ");
+     g = masuk.nextDouble();
+*/
+     //No 6 B input   
+/*
+    System.out.print("Masukan A = ");
+    a = masuk.nextDouble();
+    System.out.print("Masukan M1 = ");
+    m1 = masuk.nextDouble();
+    System.out.print("Masukan M2 = ");
+    m2 = masuk.nextDouble();
+    System.out.print("Masukan P = ");
+    p = masuk.nextDouble();
+*/
+     //No 6 C input
+/*     
+     System.out.print("Masukan PV = ");
+     pv = masuk.nextDouble();
+     System.out.print("Masukan INT = ");
+     inte = masuk.nextDouble();
+     System.out.print("Masukan YRS = ");
+     yrs = masuk.nextDouble();
+*/
+     //No 6 D input
+/*    
+     System.out.print("Masukan A = ");
+     a = masuk.nextDouble();
+     System.out.print("Masukan B = ");
+     b = masuk.nextDouble();
+     System.out.print("Masukan Cos x = ");
+     x = masuk.nextDouble();
+*/
+//No 6 A
+/*
+        s=j+v*t+((g*(t*t))/2);
+    System.out.println(s);
+*/
+
+//No 6 B
+/*   
+        g=4*(Math.PI*Math.PI)*(a*a*a)/(p*p)*((m1+m2));
+    System.out.println(g);
+*/
+
+//No 6 C
+ /*       
+        fv=pv*Math.pow(1 + inte / 100 , yrs);
+    System.out.println(fv);
+*/
+
+//No 6 D
+/*
+        c=Math.sqrt((a*a)+(b*b)-2*(a*b)*Math.cos(x));
+    System.out.println(c);
+*/
+    }
+}
